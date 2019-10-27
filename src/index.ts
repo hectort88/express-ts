1bb60f44fdb175221d1645ba1334f2763659e754
Hector Trujillo <hectort88@outlook.com>
2019-10-27 17:42:55 -0500

    CSRF support

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 require('dotenv').config()
 import path from 'path'
+import csrf from 'csurf'
 import express from 'express'
-import router from './router'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import webRouter from './routes/web'
+import apiRouter from './routes/api'
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api', apiRouter)
 app.use('/public', express.static(path.join(__dirname, '../public')))
-app.use(router)
+
+// Web routes
+app.use(cookieParser())
+app.use(csrf({ cookie: true }))
+app.use(webRouter)
 
 app.listen(port, () => { console.log(`Server running on port ${port}`) })
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -4,4 +4,6 @@ const router = express.Router()
 
 router.get('/', (req: Request, res: Response) => res.end('It works'))
 
+router.post('/', (req: Request, res: Response) => res.json({ message: 'It works' }))
+
 export default router
